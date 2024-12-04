abc7047bc9f2da60dc4077bb868a692ad4e2f9eb
Julian402 <corredor.jul@hotmail.com>
2024-12-03 22:18:27 -0500

    DepartmentRoute

--- routes/departmentRoutes.js
+++ routes/departmentRoutes.js
@@ -2,8 +2,11 @@ import express from "express";
 const router = express.Router();
 import departmentControllers from "../controllers/departmentControllers.js";
 
-router.get("/:codeDepartment", departmentControllers.getDepartmentById);
-router.get("/", departmentControllers.getDepartments);
+router.get(
+  "/getDepartmentById/:codeDepartment",
+  departmentControllers.getDepartmentById
+);
+router.get("/getDepartments", departmentControllers.getDepartments);
 router.post("/", departmentControllers.createDepartment);
 router.patch("/:codeDepartment", departmentControllers.updateDepartment);
 router.delete("/:codeDepartment", departmentControllers.deleteDepartment);
